a3dd95bc287dbc859304e5a8c57df9958afe5638
Enzo <enzo.ditaliano@gmail.com>
2020-10-31 03:05:35 -0300

    corrige layout das páginas

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <path fill="#ED7D31" fill-opacity="1" d="M0,96L48,80C96,64,192,32,288,26.7C384,21,480,43,576,48C672,53,768,43,864,37.3C960,32,1056,32,1152,42.7C1248,53,1344,75,1392,85.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
         </svg>
       </div>
-      <div className="container mt-3 mx-0 px-0">
+      <div className="container-fluid mt-3 px-0">
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/contato" component={Contato} />
