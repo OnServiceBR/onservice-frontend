3c6e13261d706e7de98d8677d67d1165e30730de
Enzo <enzo.ditaliano@gmail.com>
2020-10-31 02:54:40 -0300

    adiciona frontend da página de contato

--- src/pages/contato.page.js
+++ src/pages/contato.page.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import "../styles/contato.css";
+import sideForm from '../assets/undraw_personal_text_vkd8.png';
 
 export default class Contato extends Component {
   constructor(props) {
@@ -12,23 +14,28 @@ export default class Contato extends Component {
 
   render() {
     return(
-      <div className="Contato">
-        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-          </div>
-          <div className="form-group">
-            <label htmlFor="message">Message</label>
-            <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-      </div>
+      <div class="container-fluid" id="contact-area">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6" id="foto-contato">
+                        <img src={sideForm} alt="Imagem do formulário" />
+                    </div>
+                    <div class="col-md-6" id='contact-form'>
+                        <h2>Teve algum problema? Alguma sugestão?<br />Fale Conosco!</h2>
+                        <small class='form-text text-muted'>A OnService tem a missão de garantir satisfação total para todos os usuários.<br />
+                        Se teve qualquer tipo de problema com algum serviço contratado<br />ou se tiver quaisquer sugestões, não hesite!<br />Fale conosco.</small>
+                        <form action="" class="form-contato">
+                            <input type="text" class='form-control' placeholder="Nome Completo" name='nome_completo' />
+                            <input type="text" class='form-control' placeholder="Cidade" name='cidade' />
+                            <input type="email" class='form-control' placeholder="E-mail" name='email' />
+                            <input type="text" class='form-control' placeholder="Assunto" name='subject' />
+                            <textarea class='form-control' rows='3' placeholder="Sua mensagem..." name='message'></textarea>
+                            <input type="submit" id='procurar-btn2' value="Enviar" />
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
   }
 
@@ -45,6 +52,6 @@ export default class Contato extends Component {
   }
 
   handleSubmit(event) {
-    
+
   }
 }
