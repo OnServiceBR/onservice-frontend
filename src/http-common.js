34f3aa8fe7a3a0315be1c0f9d038167ce312e0cb
Enzo <enzo.ditaliano@gmail.com>
2020-11-19 03:29:15 -0300

    corrige arquivo com rotas para o backend

--- src/http-common.js
+++ src/http-common.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
+  // baseURL: "https://onservice-backend.herokuapp.com/",
   headers: {
     "Content-type": "application/json"
   }
