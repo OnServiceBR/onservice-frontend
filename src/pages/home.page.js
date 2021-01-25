b58af40905fceef58bf4f4173b0a8613f023fd25
Guilherme Senise <guilhermesenise.69@gmail.com>
2021-01-25 16:59:45 -0300

    adiciona 2 frases da home

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -9,7 +9,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default class Home extends Component {
   render() {
     return(
-        
+		<div>
+         <div class="homephrase">
+		  <label class="h1-home">Nunca foi tão fácil <span style={{color:"rgb(237,125,49)"}}>contratar</span> alguém!</label>
+		  
+		 </div>
+		 <div class="homephrase">
+		 <label class="h2-home">Procure um profissional para sua necessidade</label>
+		 </div>
+		 <div>
+		 <FontAwesomeIcon icon={['fas', 'search']} size="lg" color="rgb(237,125,49)"/>
+		 </div>
+		 </div>
     )
   }
 }
