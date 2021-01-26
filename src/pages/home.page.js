331055cce3636e51e1c73541da9d8b4d869d4e86
Guilherme Senise <guilhermesenise.69@gmail.com>
2021-01-25 23:57:50 -0300

    adiciona botão para a pagina contrate

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "../styles/home.css";
 import logo from '../assets/Logo - Laranja.png';
 import bonecoHome from '../assets/boneco_home.png';
@@ -10,17 +11,25 @@ export default class Home extends Component {
   render() {
     return(
 		<div>
-         <div class="homephrase">
-		  <label class="h1-home">Nunca foi tão fácil <span style={{color:"rgb(237,125,49)"}}>contratar</span> alguém!</label>
-		  
-		 </div>
-		 <div class="homephrase">
-		 <label class="h2-home">Procure um profissional para sua necessidade</label>
-		 </div>
-		 <div>
-		 <FontAwesomeIcon icon={['fas', 'search']} size="lg" color="rgb(237,125,49)"/>
-		 </div>
-		 </div>
+			<div class="homephrase">
+				<label class="h1-home">Nunca foi tão fácil <span style={{color:"rgb(237,125,49)"}}>contratar</span> alguém!</label>
+			</div>
+			<div class="homephrase">
+				<label class="h2-home">Procure um profissional para sua necessidade</label>
+			</div>
+			<div class="homephrase">
+				<form method="get" action="/contrate">
+				<button id="button1-search-home" type="submit">
+						<FontAwesomeIcon icon={['fas', 'search']} size="lg" color="rgb(237,125,49)"/>
+						&nbsp; &nbsp; Buscar por um profissional
+				</button>
+				<button id="button2-search-home" type="submit">	
+					 
+						Buscar
+				</button>
+				</form>
+			</div>
+		</div>
     )
   }
 }
