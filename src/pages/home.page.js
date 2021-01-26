5fe91d18b18ea5f64296ca242043a5d708a0b830
Enzo Dornelles Italiano <enzo.ditaliano@gmail.com>
2021-01-26 19:27:16 -0300

    Organiza um pouco o código

--- src/pages/home.page.js
+++ src/pages/home.page.js
@@ -8,24 +8,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default class Home extends Component {
   render() {
     return(
-		<div>
-			<div class="homephrase">
-				<label class="h1-home">Nunca foi tão fácil <span style={{color:"rgb(237,125,49)"}}>contratar</span> alguém!</label>
-			</div>
-			<div class="homephrase">
-				<label class="h2-home">Procure um profissional para sua necessidade</label>
-			</div>
-			<div class="homephrase">
-				<form method="get" action="/contrate">
+	<div>
+		<div class="homephrase">
+			<label class="h1-home">Nunca foi tão fácil <span style={{color:"rgb(237,125,49)"}}>contratar</span> alguém!</label>
+		</div>
+		<div class="homephrase">
+			<label class="h2-home">Procure um profissional para sua necessidade</label>
+		</div>
+		<div class="homephrase">
+			<form method="get" action="/contrate">
 				  <button id="button1-search-home" type="submit">
-						<FontAwesomeIcon icon={['fas', 'search']} size="lg" color="rgb(237,125,49)"/>
-						&nbsp; &nbsp; Buscar por um profissional
+					<FontAwesomeIcon icon={['fas', 'search']} size="lg" color="rgb(237,125,49)"/>
+					&nbsp; &nbsp; Buscar por um profissional
 				  </button>
 				  <button id="button2-search-home" type="submit">
-						Buscar
+					Buscar
 				  </button>
-				</form>
-			</div>
+			</form>
+		</div>
       <div>
       <br />
       <br />
