22fff7b91a1ac77e6be1e8056b850cd98cbd5552
Enzo <enzoo.miguel@gmail.com>
2021-05-01 23:01:30 -0300

    Organiza e renomeia alguns termos do css e js

--- src/pages/trabalhos.page.js
+++ src/pages/trabalhos.page.js
@@ -138,7 +138,7 @@ export default class Home extends Component {
           <label id="trabalhos-service-label">Serviços</label>
           <label id="trabalhos-job-label">Profissionais</label>
         </div>
-        <div class="dictionary">
+        <div class="job-dictionary">
           |
           <a href="#names-A"> A </a>|
           <a href="#names-B"> B </a>|
@@ -169,73 +169,73 @@ export default class Home extends Component {
         </div>
         <br/>
         <hr class="job-hr"/>
-         <h1 class="dictionary-letter">A<a name="names-A"></a></h1>
+         <h1 class="job-dictionary-letter">A<a name="names-A"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">B<a name="names-B"></a></h1>
+          <h1 class="job-dictionary-letter">B<a name="names-B"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">C<a name="names-C"></a></h1>
+          <h1 class="job-dictionary-letter">C<a name="names-C"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">D<a name="names-D"></a></h1>
+          <h1 class="job-dictionary-letter">D<a name="names-D"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">E<a name="names-E"></a></h1>
+          <h1 class="job-dictionary-letter">E<a name="names-E"></a></h1>
         <hr class="job-hr"/>
           <div class="job-link">
           {/* Final do link está manual */}
             <a href="/contrate/manutencao/encanador">
               <div class="row">
-                <div class="column imagecontainer">
-                  <button class="icon-button-class"> </button>
+                <div class="column job-imagecontainer">
+                  <button class="job-icon-button-class"> </button>
                 </div>
-                <div class="column textcontainer">
+                <div class="column job-textcontainer">
                   {/* Texto a ser escrito está manual, tanto título quanto quantidade de profissionais */}
                   <p class="job-name">Encanador(a)</p>
-                  <p class="workers-count">11 profissionais</p>
+                  <p class="job-workers-count">11 profissionais</p>
                 </div>
               </div>
             </a>
           </div>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">F<a name="names-F"></a></h1>
+          <h1 class="job-dictionary-letter">F<a name="names-F"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">G<a name="names-G"></a></h1>
+          <h1 class="job-dictionary-letter">G<a name="names-G"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">H<a name="names-H"></a></h1>
+          <h1 class="job-dictionary-letter">H<a name="names-H"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">I<a name="names-I"></a></h1>
+          <h1 class="job-dictionary-letter">I<a name="names-I"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">J<a name="names-J"></a></h1>
+          <h1 class="job-dictionary-letter">J<a name="names-J"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">K<a name="names-K"></a></h1>
+          <h1 class="job-dictionary-letter">K<a name="names-K"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">L<a name="names-L"></a></h1>
+          <h1 class="job-dictionary-letter">L<a name="names-L"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">M<a name="names-M"></a></h1>
+          <h1 class="job-dictionary-letter">M<a name="names-M"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">N<a name="names-N"></a></h1>
+          <h1 class="job-dictionary-letter">N<a name="names-N"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">O<a name="names-O"></a></h1>
+          <h1 class="job-dictionary-letter">O<a name="names-O"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">P<a name="names-P"></a></h1>
+          <h1 class="job-dictionary-letter">P<a name="names-P"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">Q<a name="names-Q"></a></h1>
+          <h1 class="job-dictionary-letter">Q<a name="names-Q"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">R<a name="names-R"></a></h1>
+          <h1 class="job-dictionary-letter">R<a name="names-R"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">S<a name="names-S"></a></h1>
+          <h1 class="job-dictionary-letter">S<a name="names-S"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">T<a name="names-T"></a></h1>
+          <h1 class="job-dictionary-letter">T<a name="names-T"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">U<a name="names-U"></a></h1>
+          <h1 class="job-dictionary-letter">U<a name="names-U"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">V<a name="names-V"></a></h1>
+          <h1 class="job-dictionary-letter">V<a name="names-V"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">W<a name="names-W"></a></h1>
+          <h1 class="job-dictionary-letter">W<a name="names-W"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">X<a name="names-X"></a></h1>
+          <h1 class="job-dictionary-letter">X<a name="names-X"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">Y<a name="names-Y"></a></h1>
+          <h1 class="job-dictionary-letter">Y<a name="names-Y"></a></h1>
         <hr class="job-hr"/>
-          <h1 class="dictionary-letter">Z<a name="names-Z"></a></h1>
+          <h1 class="job-dictionary-letter">Z<a name="names-Z"></a></h1>
         <hr class="job-hr"/>        
       </div>
     )
