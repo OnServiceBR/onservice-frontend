b3f2381855a25c704bbf82815a397b86090129b3
fonsecapbeatriz <fonsecapbeatriz@gmail.com>
2021-05-09 14:56:12 -0300

    Modifica como utilizar o site

--- src/pages/como-utilizar-o-site.page.js
+++ src/pages/como-utilizar-o-site.page.js
@@ -7,13 +7,28 @@ function ComoUtilizaroSite() {
             <h2 class="title-utilizarsite">Como Utilizar o Site</h2>
             <h2 class="subtitle-utilizarsite">Veja abaixo os tutoriais de como mexer no site</h2>
             <h2> </h2>
-            <div class="row">
-                <div class="col-6">
-                    <h2 class="title-utilizarsite">Tutoriais para o Cliente</h2>
+            <div class="row row-all-howtouse">
+                <div class="column column-each-howtouse">
+                    <div class="row row-title-howtouse">
+                        <h2 class="title-utilizarsite">Tutoriais para o Cliente</h2>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        Nome do bagulho 1
+                    </div>
+                    <div class="row row-video-howtouse">
+                        Vídeo do bagulho 1
+                    </div>
                 </div>
-                <hr class="howtouse-hr" />
-                <div class="col-6">
-                    <h2 class="title-utilizarsite">Tutoriais para o Profissional</h2>
+                <div class="column column-each-howtouse">
+                    <div class="row row-title-howtouse">
+                        <h2 class="title-utilizarsite">Tutoriais para o Profissional</h2>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        Nome do bagulho 2
+                    </div>
+                    <div class="row row-video-howtouse">
+                        Vídeo do bagulho 2
+                    </div>
                 </div>
             </div>
         </div >
