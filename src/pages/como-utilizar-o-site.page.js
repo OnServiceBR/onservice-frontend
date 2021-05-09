317b6d77b2bdaf031c11bff173bd2ef71f36da49
fonsecapbeatriz <fonsecapbeatriz@gmail.com>
2021-05-09 19:25:18 -0300

    Continua a desenvolver "como utilizar a página"

--- src/pages/como-utilizar-o-site.page.js
+++ src/pages/como-utilizar-o-site.page.js
@@ -3,35 +3,168 @@ import "../styles/como-utilizar-o-site.css";
 
 function ComoUtilizaroSite() {
     return (
-        <div>
-            <h2 class="title-utilizarsite">Como Utilizar o Site</h2>
-            <h2 class="subtitle-utilizarsite">Veja abaixo os tutoriais de como mexer no site</h2>
+        <div class="nomarginpage">
+            <h2 class="title-howtouse">Como Utilizar o Site</h2>
+            <h2 class="subtitle-howtouse">Veja abaixo os tutoriais de como mexer no site</h2>
             <h2> </h2>
             <div class="row row-all-howtouse">
                 <div class="column column-each-howtouse">
                     <div class="row row-title-howtouse">
-                        <h2 class="title-utilizarsite">Tutoriais para o Cliente</h2>
+                        <h2 class="titlerow-howtouse">Tutoriais para <span style={{ color: "rgb(237,125,49)" }}>Cliente</span></h2>
                     </div>
                     <div class="row row-subtitle-howtouse">
-                        Nome do bagulho 1
+                        <h2 class="topic-big-howtouse">1. <span class="topic-small-howtouse">Como criar uma conta no site</span></h2>
                     </div>
                     <div class="row row-video-howtouse">
-                        Vídeo do bagulho 1
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">2. <span class="topic-small-howtouse">O que tem na página inicial</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="https://www.youtube.com/embed/DBiNucJ64F8"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">3. <span class="topic-small-howtouse">Como alterar dados da conta (e-mail, senha)</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">4. <span class="topic-small-howtouse">Como contratar um serviço de um profissional</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">5. <span class="topic-small-howtouse">Como ver as avaliações de um profissional</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">6. <span class="topic-small-howtouse">Como avaliar um profissional</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">7. <span class="topic-small-howtouse">Onde ler os termos e condições de uso do site</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">8. <span class="topic-small-howtouse">Como entrar em contato e realizar denúncias</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
                     </div>
                 </div>
+                <div class="row-line-howtouse" />
                 <div class="column column-each-howtouse">
                     <div class="row row-title-howtouse">
-                        <h2 class="title-utilizarsite">Tutoriais para o Profissional</h2>
+                        <h2 class="titlerow-howtouse">Tutoriais para <span style={{ color: "rgb(237,125,49)" }}>Profissional</span></h2>
                     </div>
                     <div class="row row-subtitle-howtouse">
-                        Nome do bagulho 2
+                        <h2 class="topic-big-howtouse">1. <span class="topic-small-howtouse">Como criar uma conta no site</span></h2>
                     </div>
                     <div class="row row-video-howtouse">
-                        Vídeo do bagulho 2
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">2. <span class="topic-small-howtouse">O que tem na página inicial</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">3. <span class="topic-small-howtouse">Como alterar dados da conta (e-mail, senha)</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">4. <span class="topic-small-howtouse">Como editar o perfil visível aos clientes</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">5. <span class="topic-small-howtouse">Como ver as suas avaliações</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">6. <span class="topic-small-howtouse">Como assinar o Plano Premium</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">7. <span class="topic-small-howtouse">Onde ler os termos e condições de uso do site</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
+                    </div>
+                    <div class="row row-subtitle-howtouse">
+                        <h2 class="topic-big-howtouse">8. <span class="topic-small-howtouse">Como entrar em contato e realizar denúncias</span></h2>
+                    </div>
+                    <div class="row row-video-howtouse">
+                        <iframe id="player" type="text/html" width="480" height="270"
+                            src="http://www.youtube.com/embed/M7lc1UVf-VE?enablejsapi=1&origin=http://example.com"
+                            frameborder="0">
+                        </iframe>
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     );
 }
 export default ComoUtilizaroSite;
