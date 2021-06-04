cf8ec38e4e9517634c94c17097dce10ce730dbbc
fonsecapbeatriz <fonsecapbeatriz@gmail.com>
2021-06-04 14:40:48 -0300

    Finalização Alerta na Página de Perfil

--- src/pages/perfil.page.js
+++ src/pages/perfil.page.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "../styles/perfil.css";
 import swal from '@sweetalert/with-react';
 
-
+import LogoOnservice from "../assets/LogoSimboloLaranja.png";
 import Prestadorimage from "../assets/IMG-20190914-WA0056.jpg";
 import Whatsappimage from "../assets/whatsappicon.png";
 import Fotoperfil from "../assets/edinaldocirculo.jpg";
@@ -14,26 +14,26 @@ export default class Perfil extends Component {
   sendErrorAlertPerfil = (msg) => {
     swal({
       button: {
+        className: "swal-button--contact",
         text: "Entrar em contato",
+        icon: "success",
       },
       content: (
         <div>
-          <img src={Prestadorimage} width="90px" alt="Perfil" />
-          <p class="swal-title">Você está a um passo de contratar um profissional!</p>
+          <img src={LogoOnservice} width="90px" alt="Perfil" />
+          <p class="swal-title--perfil">Você está a um passo de contratar um profissional!</p>
           <p>
             {''}
           </p>
-          <p class="swal-text">Ao clicar em <span style={{ fontWeight: "bold" }}>“Entrar em Contato”</span>, você está de acordo com nosso <a href="/termos-de-uso/">Termos de Uso</a> e <a href="/politica-de-privacidade/">Política de Privacidade</a>. Para fazer qualquer tipo de reclamação ou denúncia, envie-nos uma mensagem na aba <a href="/contato/" class="swal-text--newlink">CONTATO</a>!</p>
+          <p class="swal-text--perfil">Ao clicar em <span style={{ fontWeight: "bold" }}>“Entrar em Contato”</span>, você está de acordo com nosso <a href="/termos-de-uso/">Termos de Uso</a> e <a href="/politica-de-privacidade/">Política de Privacidade</a>. Para fazer qualquer tipo de reclamação ou denúncia, envie-nos uma mensagem na aba <a href="/contato/" class="swal-text--newlink">CONTATO</a>!</p>
         </div>
       )
     })
   }
 
-
   messageContrate = () => {
     this.sendErrorAlertPerfil("")
   }
-
 
   render() {
     return (
