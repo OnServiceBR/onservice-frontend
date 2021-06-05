af6e313c8218459b2562229e9780ea9a618e7e37
fonsecapbeatriz <fonsecapbeatriz@gmail.com>
2021-06-05 00:08:48 -0300

    Melhoria finalizada na página perfil

--- src/pages/perfil.page.js
+++ src/pages/perfil.page.js
@@ -5,8 +5,6 @@ import swal from '@sweetalert/with-react';
 import LogoOnservice from "../assets/LogoSimboloLaranja.png";
 import Prestadorimage from "../assets/IMG-20190914-WA0056.jpg";
 import Whatsappimage from "../assets/whatsappicon.png";
-import Fotoperfil from "../assets/edinaldocirculo.jpg";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 export default class Perfil extends Component {
@@ -39,10 +37,40 @@ export default class Perfil extends Component {
     return (
       <div class="row row-information-center">
         <div class="column column-perfilimage">
-          <img src={Prestadorimage} width="300px" alt="Perfil" />
+          <div class="row row-perfilimage">
+            <img src={Prestadorimage} width="300px" alt="Perfil" />
+          </div>
+        </div>
+        <div class="column column-perfilinformation">
+          <div class="row row-main-information-perfil">
+            <div class="column column-perfilcircleimage">
+              <div class="row row-perfilcircleimage">
+                <img src={Prestadorimage} alt="Perfil" />
+              </div>
+            </div>
+            <div class="column column-perfilnameandbutton">
+              <div class="row row-perfilnameandbutton">
+                <a>Ednaldo Pereira</a>
+                <hr class="perfil-hr" />
+                <button type='button'
+                  onClick={
+                    this.messageContrate.bind(this)
+                  } class="button-entraremcontato">
+                  Entrar em contato
+                      <img id="buttonimageperfil" src={Whatsappimage} width="22px" alt="Perfil" />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="row row-professionperfil">
+            <p> Cantor</p>
+            <p> Compositor</p>
+            <div class="perfil-description">
+              <label class="perfil-description-text">O incentivo ao avanço tecnológico, assim como o início da atividade geral de formação de atitudes acarreta um processo de reformulação e modernização das condições financeiras e administrativas exigidas.Caros amigos, a complexidade dos estudos efetuados exige a precisão e a definição dos métodos utilizados na avaliação de resultados. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a consulta aos diversos militantes.</label>
+            </div>
+          </div>
         </div>
       </div>
-
     )
   }
 }
