cfd60148c090382b0e77799a941fcf752bc2d017
fonsecapbeatriz <fonsecapbeatriz@gmail.com>
2021-07-02 19:37:36 -0300

    Adiciona destaques na home
    colocar os prestadores no destaque da home, fazendo passar um link pro cards e criando um maps

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,20 +2,20 @@ import React from "react";
 
 const Card = (props) => {
     return (
-    <div class="row row-all-cards" id="cards-change-color-on-hover">
-        <a href="/perfil" class="column column-each-card">
-            <div class="row row-card-work">
-                <p>{props.Job}</p>
-            </div>
-            <hr class="each-card-hr"/>
-            <div class="row row-card-picture">
-                <img alt="" src={props.Picture}/>
-            </div>
-            <div class="row row-card-name">
-                <p>{props.Name}</p>
-            </div>
-        </a>
-    </div>
+        <div class="row row-all-cards" id="cards-change-color-on-hover">
+            <a href={props.Link} class="column column-each-card">
+                <div class="row row-card-work">
+                    <p>{props.Job}</p>
+                </div>
+                <hr class="each-card-hr" />
+                <div class="row row-card-picture">
+                    <img class="emphasize-picture-profissões" alt="" src={props.Picture} />
+                </div>
+                <div class="row row-card-name">
+                    <p>{props.Name}</p>
+                </div>
+            </a>
+        </div>
     )
 }
 
