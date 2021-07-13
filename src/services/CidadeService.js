be01d443e7d27d4202a23f8828e802aca4aad2ab
Enzo Italiano <enzo.ditliano@gmail.com>
2021-07-13 14:42:27 -0300

    Configura para envio de prestadores na pagina anuncie sem imagem

---
+++ src/services/CidadeService.js
@@ -0,0 +1,33 @@
+import firebase from "../firebase";
+
+const db = firebase.ref("/cidades");
+
+const getAll = () => {
+  return db;
+};
+
+const create = (data) => {
+  return db.push(data);
+};
+
+const update = (key, data) => {
+  return db.child(key).update(data);
+};
+
+const remove = (key) => {
+  return db.child(key).remove();
+};
+
+const removeAll = () => {
+  return db.remove();
+};
+
+const Cidade = {
+  getAll,
+  create,
+  update,
+  remove,
+  removeAll,
+};
+
+export default Cidade;
